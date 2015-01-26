0d29d9ac9415bf718057f5605352e901ac7e8d7d
Srikanth Gomatam <srikanth.gomatam@bloomingdales.com>
2015-01-26 11:32:48 -0500

    adding POM file to trigger mvn builds

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function(grunt) {
+  var destination =  "target",
+  	  source = "./";
+
   grunt.initConfig ({
     sass: {
       dist: {
@@ -14,6 +17,7 @@ module.exports = function(grunt) {
         }]
       }
     },
+
     watch: {
       source: {
         files: ['sass/**/*.scss'],
@@ -22,12 +26,43 @@ module.exports = function(grunt) {
           livereload: true, // needed to run LiveReload
         }
       }
-    }
+    },
+
+    clean: {
+	  all: destination,
+	  options: {
+	    force: true
+	  }
+	},
+
+	copy: {
+	  client: {
+	    files: [
+	      {
+	        expand: true,
+	        cwd: source,
+	        src: ['**'],
+	        dest: destination + "/"
+	      }
+	    ]
+  	  }
+	}
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['sass']);
+  
+  //grunt.registerTask('default', ['sass']);
+  
+  grunt.registerTask('default', 'build');
+	
+  grunt.registerTask('build', 'Builds a development version of the app.', function() {
+    grunt.task.run([
+      'clean',
+      //'sass',
+      'copy'
+    ]);
+  });
 
-}
+};
