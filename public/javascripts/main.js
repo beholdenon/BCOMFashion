82f4cd2bf9228034322bc12facb6f034cf48bb2d
Srikanth Gomatam <srikanth.gomatam@bloomingdales.com>
2015-04-09 00:27:42 -0400

    MCOMRE-98711 - changing node server templating logic from jade to html
    (cherry picked from commit 2598fd9d36bd5a086fcca9a0f55089213ddcf34a)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,45 @@
+/**
+ *
+ * Need to move the requirejs-config section to a different file
+ * Currently it's here because, this file loaded thru data-main in index.html
+ * 
+ */
+
+(function() {
+    var paths = {
+        "backbone": "libs/backbone",
+        "handlebars": "libs/handlebars",
+        "jquery": "libs/jquery",
+        "require": "libs/requirejs",
+        "underscore": "libs/lodash"
+    },
+    shim = {
+        "backbone": {
+            "deps": ["underscore", "jquery"],
+            "exports": "Backbone"
+        },
+        "handlebars": {
+            "exports": "Handlebars"
+        }
+    };
+
+    require.config({
+        paths: paths,
+        shim: shim
+    });
+})();
+
 define([
   'backbone',
   'jquery',
-  'underscore'
+  "underscore"
 ], function(Backbone, $, _) {
 
   // Globally namespaced objects, please keep to absolute minimum
   window.App = {};
 
-  new AppRouter();
+  //new AppRouter();
+  console.log("erererer");
 
   Backbone.history.start({
     pushState: true//,
