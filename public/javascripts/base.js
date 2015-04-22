d0d2fc73b0cc8cbee1a2abc9e9a686b44d2c1ab4
Constantin <constantin.bigu@bloomingdales.com>
2015-04-22 11:40:44 -0400

    MCOMRE-98711  - Foundation-based responsive header & footer

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -4,198 +4,6 @@ define([
   'underscore'
 ], function(Backbone, $, _) {
 	
-	BLOOMIES.buildMobileHeader = function() {
-		var myvar = '<section id="mw-region-header"><header id="mw-header-container"><div id="mw-nav-button"><i id="mw-nav-button-icon" class="icon-menuButton"></i></div>'+
-					'<div id="mw-bag-status">'+
-					'      <div id="myBagLink" class=" mw_myBagLink to_be_sprited-t_bag">'+
-					'        <a href="http://m.bloomingdales.com/bag/index.ognc" id="qb_addToBagOverlayLabel" class="qb_closedOverlayLabel">'+
-					'          <span id="brownBagItemsTotal">'+
-					'            </span>'+
-					'        </a>'+
-					'      </div>'+
-					'    </div>'+
-					'    <div class="mvHiddenInRegistry" id="bl_nav_top_logo">'+
-					'      <a href="http://m.bloomingdales.com/?cm_sp=NAVIGATION-_-TOP_NAV-_-TOP_BLOOMIES_ICON"><img src="//assets.moovweb.net/bloomingdales/bcom-origin-prod/v30/images/bloomingdales.png" id="mw-header-logo-image">'+
-					'      </a>'+
-					'    </div>'+
-					'    <div id="r_nav_top_logo" class="mvHiddenInSite">'+
-					'      <a href="#" class="to_be_sprited-registryHeader">'+
-					'      </a>'+
-					'    </div>'+
-					'  </header></section>'+
-					'  <section id="mw-search-wrapper"><div class="mvHeaderWrapper">'+
-					'    <div class="mvBDHeader">'+
-					'      <div class="bl_nav_top_sub_search">'+
-					'        <div id="mvBDSearchFormToggle">'+
-					'          <form id="keywordSearch" method="get" action="http://m.bloomingdales.com/shop/search">'+
-					'            <div class="bl_nav_top_sub_search_input">'+
-					'              <div class="bl_nav_top_sub_search_go">'+
-					'                <span class="mw_go_icon to_be_sprited-t_glass"></span>'+
-					'                <input class="bl_nav_top_sub_search_go" type="submit" alt="Search Now" value=" " name="KEYWORD_GO_BUTTON">'+
-					'  </div>'+
-					'              <input type="hidden" value="*" name="SearchTarget"><input class="aboveNavSearchBox  ui-autocomplete-input" type="text" maxlength="72" name="Keyword" value="" title="Keyword Search" id="mvSearchField" placeholder="Search" autocapitalize="off" autocorrect="off" autocomplete="off"><span class="clearSearchBoxIcon hide"></span>'+
-					'              <input type="hidden" value="mobile" name="channel"><input type="hidden" value="20" name="ppp">'+
-					'  </div>'+
-					'            <div class="clearLeft">'+
-					'            </div>'+
-					'          </form>'+
-					'        </div>'+
-					'      </div>'+
-					'    </div>'+
-					'</div></section>';
-	
-	 	$("header").html(myvar);
-	};
-	
-	BLOOMIES.buildMobileFooter = function() {
-		
-		var myvar = '<div class="footer-container">'+
-					'	<div id="footer-links">'+
-					'		<span id="footer-sign-in" class="footer-sign-link">'+
-					'			<a href="http://www.bloomingdales.com/account/myaccount?cm_sp=NAVIGATION--TOP_NAV--MY_ACCOUNT">Sign In</a>'+
-					'		</span>'+
-					'		<span id="footer-sign-out" class="footer-sign-link displayNone">'+
-					'			<a href="http://www.bloomingdales.com/signin/signout.ognc">Sign Out</a>'+
-					'		</span>'+
-					'		<span id="footer-sign-up" class="footer-sign-link"> '+
-					'			<a href="http://www.bloomingdales.com/signup">Sign up for emails & texts</a>'+
-					'		</span>'+
-					'		<div class="clearboth"></div>'+
-					'	</div>'+
-					''+
-					'	<div id="footer-social-links">'+
-					'		<span>'+
-					'			<a class="footer-icon footer-facebook" href="http://www.facebook.com/Bloomingdales?cm_sp=FOOTER-_-BOTTOM_NAV-_-FACEBOOK"></a>'+
-					'		</span>'+
-					'		<span>'+
-					'			<a class="footer-icon footer-twitter" href="http://mobile.twitter.com/Bloomingdales?cm_sp=FOOTER-_-BOTTOM_NAV-_-TWITTER"></a>'+
-					'		</span>'+
-					'		<span>'+
-					'			<a class="footer-icon footer-pin" href="http://www.pinterest.com/Bloomingdales?cm_sp=FOOTER-_-BOTTOM_NAV-_-PINTEREST"></a>'+
-					'		</span>'+
-					'		<span>'+
-					'			<a class="footer-icon footer-instagram" href="http://instagram.com/bloomingdales#?cm_sp=FOOTER-_-BOTTOM_NAV-_-INSTAGRAM"></a>'+
-					'		</span>'+
-					'		<span id="app-line">'+
-					'			<a id="footer-app-ios" class="footer-icon footer-phone displayNone" href="itms-apps://itunes.apple.com/us/app/bloomingdales-big-brown-bag/id525536985?mt=8">Download the Big Brown Bag App</a>'+
-					'			<a id="footer-app-android" class="footer-icon footer-phone " href="https://play.google.com/store/apps/details?id=com.bloomies.android">Download the Big Brown Bag App</a>'+
-					'		</span>'+
-					'		'+
-					'		<div class="clearboth"></div>'+
-					'	</div>'+
-					''+
-					'	<div id="footer-bottom-links">'+
-					'		<span class="customer-service">'+
-					'			'+
-					'			<a href="https://customerservice.bloomingdales.com/app/home/?cm_sp=NAVIGATION-_-BOTTOM_NAV-_-CUSTOMER_SERVICE" data-environment="prod">Customer Service</a>'+
-					'		</span>'+
-					'		<span class="separator"></span>'+
-					'		<span class="fullSite"> '+
-					'			<a id="mw_desktop_link" class="desktop_site" href="http://www.fashion.bloomingdales.com/fashion-index/premium-shoes-handbags-spring-collections-2015.jsp?stop_mobi=yes&&cm_sp=NAVIGATION-_-TOP_NAV-_-16961-CATEGORY_ICON_IMAGE-Premium-Designer-Lookbook">Full Site</a>'+
-					'		</span>'+
-					'		<span class="separator"></span>'+
-					'		<span class="country"> '+
-					'			<a id="mw_country_link" class="desktop_site" href="http://www1.bloomingdales.com/internationalContext/index.ognc?stop_mobi=yes&cm_sp=NAVIGATION_MEW-_-BOTTOM_LINKS-_-INTERNATIONAL_SHOPPING">Choose Your Country</a>'+
-					'		</span>'+
-					'		<br><span class="careers">'+
-					'			<a href="http://www.bloomingdalesjobs.com">Careers</a> '+
-					'		</span>'+
-					'		<span class="separator"></span>'+
-					'		<span class="privacy-practice">'+
-					'			'+
-					'			<a href="https://customerservice.bloomingdales.com/app/answers/detail/a_id/769/?cm_sp=NAVIGATION-_-BOTTOM_LINKS-_-PRIVACY_PRACTICES" data-environment="prod">Privacy Practices</a>'+
-					'		</span>'+
-					'		<span class="separator"></span>'+
-					'		<span class="bill">'+
-					'			<a href="http://www.macysinc.com/social-responsibility/customer-bill-of-rights/default.aspx">Customers\' Bill of Rights</a>'+
-					'		</span>'+
-					'	</div>'+
-					''+
-					'	<div id="footer-copy-right"> '+
-					'		© 2015 Bloomingdale\'s, dba bloomingdales.com.'+
-					'	</div>'+
-					''+
-					'</div>';
-		
-		$("footer").html(myvar);
-	};
-	
-	BLOOMIES.buildMobileNav = function() {
-		var myvar = '<nav id="mw-nav-container"><ul id="mw-nav-menu" class="nav nav-tabs nav-stacked menu" data-animating="false">'+
-					'<li id="top" class="headerRow fobtop">'+
-					'      <a class="mw-nav-link">'+
-					'        Menu'+
-					'      </a>'+
-					'    </li>'+
-					'    <li id="shop" class="fobshop firstlevel">'+
-					'      <a class="mw-nav-link" href="http://m.bloomingdales.com/">'+
-					'        Shop'+
-					'      </a>'+
-					'    </li>'+
-					'    <li id="deals" class="fobdeals firstlevel">'+
-					'      <a class="mw-nav-link" href="http://m.bloomingdales.com/shop/sales-offers?cm_sp=NAVIGATION-_-TOP_NAV-_-3977-SALES-%26-OFFERS-See-All">'+
-					'        Promotions'+
-					'      </a>'+
-					'    </li>'+
-					'    <li id="stores" class="fobstores firstlevel">'+
-					'      <a class="mw-nav-link" href="http://m.bloomingdales.com/store/index.ognc?cm_sp=NAVIGATION--TOP_NAV--STORES_EVENTS">'+
-					'        Stores'+
-					'      </a>'+
-					'    </li>'+
-					'    <li id="myAccount" class="fobmyAccount firstlevel">'+
-					'      <a class="mw-nav-link" href="https://m.bloomingdales.com/account/myaccount?cm_sp=navigation--top_nav--account">'+
-					'        My Account'+
-					'      </a>'+
-					'    </li>'+
-					'    <li id="mybWallet" class="fobmybWallet firstlevel">'+
-					'      <a class="mw-nav-link" href="https://m.bloomingdales.com/account/wallet?ocwallet=true&cm_sp=NAVIGATION_MEW--SIDE_NAV--MY_WALLET-n-n">'+
-					'        MY bWALLET'+
-					'      </a>'+
-					'    </li>'+
-					'    <li id="loyallist" class=" fobloyallist firstlevel">'+
-					'      <a class="mw-nav-link">'+
-					'        My Loyallist'+
-					'      </a>'+
-					'    </li>'+
-					'    <ul id="loyallist_children" class="no-show children_wrapper">'+
-					'<li id="viewpoints" class="fobloyallist children">'+
-					'        <a class="mw-nav-link" href="https://m.bloomingdales.com/loyallist/accountsummary">'+
-					'          View My Points'+
-					'        </a>'+
-					'      </li>'+
-					'      <li id="enroll" class="fobloyallist children">'+
-					'        <a class="mw-nav-link" href="https://m.bloomingdales.com/loyallist">'+
-					'          Enroll'+
-					'        </a>'+
-					'      </li>'+
-					'    </ul>'+
-					'<li id="registry" class="fobregistry firstlevel">'+
-					'      <a class="mw-nav-link">'+
-					'        The Registry'+
-					'      </a>'+
-					'    </li>'+
-					'    <ul id="registry_children" class="no-show children_wrapper">'+
-					'<li id="find" class="fobregistry children">'+
-					'        <a class="mw-nav-link" href="http://m.bloomingdales.com/registry/wedding/registrysearch?cm_re=give--n--n">'+
-					'          Find'+
-					'        </a>'+
-					'      </li>'+
-					'      <li id="manage" class="fobregistry children">'+
-					'        <a class="mw-nav-link" href="http://m.bloomingdales.com/registry/wedding/registrymanager">'+
-					'          Manage'+
-					'        </a>'+
-					'      </li>'+
-					'      <li id="create" class="fobregistry children">'+
-					'        <a class="mw-nav-link" href="http://m.bloomingdales.com/registry/wedding/registrycaptureemail?cm_re=register--n--n">'+
-					'          Create'+
-					'        </a>'+
-					'      </li>'+
-					'    </ul>'+
-					'</ul></nav>';
-					
-		$("#bl_main_container").prepend(myvar);
-	};
-	
 	function toggleChildren (id_selector) {
 	    $(id_selector).addClass("currentRow");
 	    $(id_selector+"_children").slideDown("slow");
@@ -345,16 +153,35 @@ define([
 	            navToggle();
 	        }
 	    });
+		
+		var placeholder = $(".aboveNavSearch .aboveNavSearchInput input").attr('placeholder');
+	    $('#aboveNav .headerTablet > a:nth-child(1)').on("click", function() {
+	    	$('#aboveNavBanner').hide();
+	    	$('.headerTablet').removeClass('show-for-medium').hide();
+	    	$('.aboveNavSearch').removeClass('show-for-large-up').show();
+
+	    	$('.aboveNavSearch .aboveNavSearchInput input').focus();
+		    if ($('.aboveNavSearch .aboveNavSearchInput input').val() == '') {
+		        $('.aboveNavSearch .aboveNavSearchInput input').val(placeholder);
+		    }
+	    });
+		$('.aboveNavSearch .aboveNavSearchInput input').on('keypress', function() {
+		    if ($(this).val() == placeholder) {
+		        $(this).val('');
+		    }
+		});
+		$('.aboveNavSearch .aboveNavSearchInput input').on('blur', function() {
+	    	event.preventDefault();
+	    	$('#aboveNavBanner').show();
+	    	$('.headerTablet').addClass('show-for-medium').show();
+	    	$('.aboveNavSearch').addClass('show-for-large-up').hide();
+	    	$('.aboveNavSearch .aboveNavSearchInput input').val('');
+		});
 	};
 	
 	var initMobile = function(){
-		if(BLOOMIES.isMEW) {
-			BLOOMIES.buildMobileHeader();
-	    	BLOOMIES.buildMobileFooter();
-	    	BLOOMIES.buildMobileNav();
-	    	BLOOMIES.initMobileNav();
-	    	$('#brownBagItemsTotal').html(thisUser.getCartItemTotal());
-		}
+    	BLOOMIES.initMobileNav();
+    	$('#brownBagItemsTotal').html(thisUser.getCartItemTotal());
 	};
 	return initMobile;
 	
