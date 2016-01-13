b9fa9413bbb1978cadd1d3883945b8bdc5040dc9
Constantin <constantin.bigu@bloomingdales.com>
2016-01-13 15:14:37 -0500

    B-36737: modified gruntfile vertical template

--- gruntfile.js
+++ gruntfile.js
@@ -350,7 +350,7 @@ module.exports = function(grunt) {
                         nodemon.on('config:update', function() {
                             // Delay before server listens on port
                             setTimeout(function() {
-                                require('open')('http://localhost:3000/template-horizontal/');
+                                require('open')('http://localhost:3000/template-vertical/');
                             }, 1000);
                         });
 
