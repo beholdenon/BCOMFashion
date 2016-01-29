f6ed4cfae668f50f14f511feb66666d3c05173a0
Michael Curtis <michael.curtis@bloomingdales.com>
2016-01-29 17:23:47 -0500

    B-39283 Adding lookbook slideshow, bloomoji page, taxi animations and overlays.

--- public/javascripts/projects/100-percent/spring-2016.js
+++ public/javascripts/projects/100-percent/spring-2016.js
@@ -2,76 +2,165 @@
 
 $(document).ready( function($) {
 
+	// ===== Init Actions =====
+
 	$('header, footer').remove();
 	
-	// driveTaxi();
+	driveTaxi();
 
-	// setInterval( function(){
-	// 	driveTaxi();
-	// }, 10000);
+	setInterval( function(){ driveTaxi(); }, 14000);
 
-	// $('#taxi').on('click', function () {
-	// 	$('#')
-	// });
-
-	$('.window').on('click', function () {
-		$('#lookbookSlideShow').show();
+	$('#taxi').on('click', function () {
+		$('#taxiOverlay').show();
 	});
 
-	$('#overlayBg, #overlayClose').on('click', function () {
-		$('#lookbookSlideShow').hide();
+	// ===== Page Interactions =====
+
+	// closed the taxi overlay
+	$('#overlayBg, .close').on('click', function () {
+		$('#taxiOverlay').hide();
 	});
 
-	$('#campaignLink').on('click', function () {
-		$('#storeBg, #lookbooks, .window').hide();
-		$('#about').show();
-		$('#campaignLink').addClass('active');
-	});
-
+	// clicked the main logo
 	$('.link .logo').on('click', function () {
-		$('#about, #lookbooks').hide();
+		$('#about, #lookbooks, #bloomoticons').hide();
 		$('#storeBg, .window').show();
+		$('#topNav').addClass('main');
 		$('.link').removeClass('active');
+		$('body').removeAttr('style');
+		$('#taxi').show();
 	});
 
-	$('#lookbookLink').on('click', function () {
-		$('#storeBg, #about, .window').hide();
+	// clicked About the Campaign
+	$('#campaignLink').on('click', function () {
+		$('#storeBg, #lookbooks, .window, #bloomoticons').hide();
+		$('#about').show();
+		$('#topNav').removeClass('main');
+		$('.link').removeClass('active');
+		$('#campaignLink').addClass('active');
+		$('body').css('background-color', '#E6EAEC');
+		$('#taxi').hide();
+	});
+
+	// clicked Bloomoji nav link
+	$('#mojiLink').on('click', function () {
+		$('#storeBg, #lookbooks, .window, #about').hide();
+		$('#bloomoticons').show();
+		$('#topNav').removeClass('main');
+		$('.link').removeClass('active');
+		$('#mojiLink').addClass('active');
+		$('body').css('background-color', '#DADAD8');
+		$('#taxi').hide();
+	});
+
+	// clicked Lookbook in the top nav
+	$('#lookbookDefaultLink').on('click', function () {
+		lookbookSetup();
+
+		$('.holdingPen .slide').clone().appendTo( '#slideBox' );
+		$('#slideBox .slide:first-child').addClass('active');
+	});
+
+	// clicked on womens in the lookbook dropdown
+	$('#lookbookDropdown .women').on('click', function() {
+		lookbookSetup();
+		$('.holdingPen.women .slide').clone().appendTo( '#slideBox' );
+		$('#slideBox .slide:first-child').addClass('active');
+	});
+
+	// clicked on mens in the lookbook dropdown
+	$('#lookbookDropdown .men').on('click', function() {
+		lookbookSetup();
+		$('.holdingPen.men .slide').clone().appendTo( '#slideBox' );
+		$('#slideBox .slide:first-child').addClass('active');
+	});
+
+	// clicked on home in the lookbook dropdown
+	$('#lookbookDropdown .home').on('click', function() {
+		lookbookSetup();
+		$('.holdingPen.home .slide').clone().appendTo( '#slideBox' );
+		$('#slideBox .slide:first-child').addClass('active');
+	});
+
+	// clicked on a store window
+	$('.window').on('click', function () {
+		lookbookSetup();
+
+		$('.holdingPen .slide').clone().appendTo( '#slideBox' );
+		$('#slideBox .slide').eq( $(this).attr('data-window') ).addClass('active');
+	});
+
+	function lookbookSetup() {
+		$('#storeBg, #about, .window, #bloomoticons').hide();
 		$('#lookbooks').show();
-		$('#lookbookLink').addClass('active');
-	});
+		$('.link').removeClass('active');
+		$('#lookbookDefaultLink').addClass('active');
+		$('#topNav').removeClass('main');
+		$('#slideBox').html('');
+		$('#taxi').hide();
+	}
 
+	// clicked on a slide show arrow
 	$('.arrow').on('click', function() {
-		var position =  $(this).parent().find('.active').attr('id').substring($(this).parent().find('.active').attr('id').indexOf('-')+1);
+		var position =  $('#slideBox .active').index(),
+			next;
 
 		var slide = {
 
-			left: function (p) {
-				$( '#lookbookPage-' + (parseInt(p)+1) ).css('left','-100vw').show();
-				$( '#lookbookPage-' + (parseInt(p)+1) + ', #lookbookPage-' + p ).animate({
-					'left': '+=100vw'
-				},600);
+			left: function (p, n) {
+
+				if (n < 0) n = $('#slideBox .slide').length - 1;
+
+				$('#slideBox .slide').eq(n).css('left','-100vw').show();
+				var target = $('#slideBox .slide').filter( function (i){
+					return $.inArray(i, [p,n]) > -1;
+				}) ;
+
+				target.animate({'left': '+=100vw'},600, function () {
+					$('#slideBox .active').removeClass('active');
+					$('#slideBox .slide').eq(n).addClass('active');
+					$('.arrow').removeClass('active');
+				});
 			},
 
-			right: function (p) {
-				$( '#lookbookPage-' + (parseInt(p)+1) ).css('left','100vw').show();
-				$( '#lookbookPage-' + (parseInt(p)+1) + ', #lookbookPage-' + p ).animate({
-					'left': '-=100vw'
-				},600);
+			right: function (p, n) {
+
+				if (n >= $('#slideBox .slide').length) n=0;
+
+				$('#slideBox .slide').eq(n).css('left','100vw').show();
+				var target = $('#slideBox .slide').filter( function (i){
+					return $.inArray(i, [p,n]) > -1;
+				}) ;
+
+				target.animate({'left': '-=100vw'},600, function () {
+					$('#slideBox .active').removeClass('active');
+					$('#slideBox .slide').eq(n).addClass('active');
+					$('.arrow').removeClass('active');
+				});
 			}
 
 		};
 
-		if ( $(this).hasClass('left') ) {slide.left(position);} else {slide.right(position);}
+		if ( !$(this).hasClass('active')) {
+			$('.arrow').addClass('active');
+			if ( $(this).hasClass('left') ) {
+				next = position-1;
+				slide.left(position, next);
+			} else {
+				next = position+1;
+				slide.right(position, next);
+			}
+		}
 
 	});
 
 
 });
 
-// function driveTaxi() {
-// 	$('#taxi').animate({
-// 		left: '200%'
-// 	}, 10000, function() {
-// 		$('#taxi').css('left', '-100%');
-// 	});
-// }
+function driveTaxi() {
+	$('#taxi').animate({
+		left: '200%'
+	}, 10000, function() {
+		$('#taxi').css('left', '-100%');
+	});
+}
