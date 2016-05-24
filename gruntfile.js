55ade0736e37c9f74c3c698b23e435868aad37f7
Jerry Han <jerry.han@bloomingdales.com>
2016-05-24 17:34:14 -0400

    B-46929 first commit
    Clean start from vertical template

--- gruntfile.js
+++ gruntfile.js
@@ -350,7 +350,7 @@ module.exports = function(grunt) {
                         nodemon.on('config:update', function() {
                             // Delay before server listens on port
                             setTimeout(function() {
-                                require('open')('http://localhost:3000/lookbooks/outfit-ideas-stylish-summer/');
+                                require('open')('http://localhost:3000/lookbooks/summer-party-dining-guide-2016/');
                             }, 1000);
                         });
 
@@ -468,7 +468,7 @@ module.exports = function(grunt) {
             dev: [
                 'nodemon',
                 'watch',
-                'node-inspector',
+                //'node-inspector',
                 'notify:build'
             ],
             options: {
