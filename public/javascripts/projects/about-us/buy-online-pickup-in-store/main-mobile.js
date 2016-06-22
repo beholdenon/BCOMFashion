ecb288d9629f3560eb21e86869d88a1c0ab170f9
Constantin Bigu <constantin.bigu@bloomingdales.com>
2016-06-22 14:41:25 -0400

    B-51709: BOPS Page Creation Mobile and Desktop (4)

--- public/javascripts/projects/about-us/buy-online-pickup-in-store/main-mobile.js
+++ public/javascripts/projects/about-us/buy-online-pickup-in-store/main-mobile.js
@@ -1,48 +1,10 @@
-'use strict';
+(function() {
+    'use strict';
 
-(function(window, $) {
+    var categoryID = 'mbl:Ways_to_Shop',
+        pageID = 'mbl:bops';
 
-    var BOPS = {
-        cmCat: 'mbl:Ways_to_Shop',
-        cmPageId: 'mbl:bops',
-        cmTag: 'ways_to_shop-_-bops-_-',
-
-        init: function () {
-            var self = this;
-            self.coremetrics('Pageview', self.cmCat, self.cmPageId);
-        },
-
-        coremetrics: function (tagType, categoryID, pageID) {
-            var self = this;
-
-            if (tagType === 'Pageview') {
-                try {
-                    window.BLOOMIES.coremetrics.cmCreatePageviewTag(pageID, categoryID);
-                } catch (e) {
-                    self.logErr('CoreM_err: ' + e);
-                }
-                self.logErr('CoreM ::: tagType: Pageview; categoryID: ' + categoryID + '; pageID: ' + pageID);
-            } else if (tagType === 'Element') {
-                try {
-                    window.BLOOMIES.coremetrics.cmCreatePageElementTag(pageID, categoryID);
-                } catch (e) {
-                    self.logErr('CoreM_err: ' + e);
-                }
-
-                self.logErr('CoreM ::: tagType: Element; categoryID: ' + categoryID + '; pageID: ' + pageID);
-            }
-        },
-
-        logErr: function (log) {
-            //log errors only in DEV mode
-            if (window.location.href.indexOf('fashion.bloomingdales.com') < 0) {
-                window.console.info(log);
-            }
-        }
-    };
-
-    $(window).load(function() {
-        BOPS.init();
-    });
-
-}(window, jQuery));
+    window.onload = function() {
+        window.BLOOMIES.coremetrics.cmCreatePageviewTag(pageID, categoryID);
+    };   
+}());
