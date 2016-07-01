1ca5593260e6eff2ccac84a276f3c14fcd742005
Leonardo Brandao <lbrandao@avenuecode.com>
2016-07-01 12:19:13 -0300

    B-53645 - DEFECT: Syndeca Integration Script on Mobile

--- public/javascripts/projects/lookbooks/syndeca-projects/main-desktop.js
+++ public/javascripts/projects/lookbooks/syndeca-projects/main-desktop.js
@@ -4,7 +4,7 @@ window.SYNDECA.vertical = (function(window, document, $) {
     'use strict';
 
     //back to top listener
-    $('.desktop_back_to_top').on('click', function() {
+    $('.desktop_back_to_top, #syndeca_mobile_back_to_top_button').on('click', function() {
         $('html, body').animate({
             scrollTop: 0
         }, 'slow' );
