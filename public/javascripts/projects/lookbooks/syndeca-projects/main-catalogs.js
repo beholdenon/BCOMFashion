8741712b76c9f1edbf396df90f6eb42b61cf5537
Leonardo Brandao <lbrandao@avenuecode.com>
2016-08-05 17:51:39 -0300

    B-56617 - Syndeca Clean-up (2)

---
+++ public/javascripts/projects/lookbooks/syndeca-projects/main-catalogs.js
@@ -0,0 +1,12 @@
+window.SYNDECA = window.SYNDECA || {};
+
+window.SYNDECA.catalogs = (function(window, document, $) {
+    'use strict';
+
+
+    $(window).load(function() {
+    	
+        window.BLOOMIES.coremetrics.cmCreatePageviewTag('SyndecaCatalog', 'SyndecaCatalog');
+    });
+
+}(window, document, jQuery));
