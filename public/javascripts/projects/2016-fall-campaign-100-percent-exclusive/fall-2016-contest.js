c85b094db0671a2d2768d455f9d048768f39f5f6
Jerry Han <jerry.han@bloomingdales.com>
2016-08-12 14:58:58 -0400

    B-54215 (10)
    contest desktop page done, minor fixes, nav b aligned.

--- public/javascripts/projects/2016-fall-campaign-100-percent-exclusive/fall-2016-contest.js
+++ public/javascripts/projects/2016-fall-campaign-100-percent-exclusive/fall-2016-contest.js
@@ -4,6 +4,22 @@ $( window ).load(function() {
 //===============================================================================================================//
 
 	'use strict';
+	$.fn.coreTag('Pageview', 'fall16_100percent--sweeps');
+
+
+	if($(window).height()>=1030){
+		$('#contest_desktop').css({
+			'height':$(window).height()+'px'
+		});			
+	}
+
+	$( window ).on('resize',function() {
+		if($(window).height()>=1030){
+			$('#contest_desktop').css({
+				'height':$(window).height()+'px'
+			});			
+		}
+	});
 
 
 
@@ -23,7 +39,7 @@ $( window ).load(function() {
 
 
 
-
+/*
 
 		$( '.download' ).click(function() {
 		    this.href = $('#canvasBloom2')[0].toDataURL();
@@ -166,12 +182,12 @@ $( window ).load(function() {
 	            update = true;
 	        });
 
-	        bitmap.on("rollover", function (/*evt*/) {
+	        bitmap.on("rollover", function () {
 	            this.scaleX = this.scaleY = this.scale * 1.2;
 	            update = true;
 	        });
 
-	        bitmap.on("rollout", function (/*evt*/) {
+	        bitmap.on("rollout", function () {
 	            this.scaleX = this.scaleY = this.scale;
 	            update = true;
 	        });
@@ -188,150 +204,6 @@ $( window ).load(function() {
 		        stage.update(event);
 		    }
 		}
-
-
-
-
-
-/*
-
-	var element = {
-		'elementID': '',
-		'elementCategory': 'spring16_100percent',
-	};
-	
-	window.BLOOMIES.coremetrics.pageViewExploreAttributes = new window.BLOOMIES.coremetrics.exploreAttributes();
-
-	var find = '/';
-	var re = new RegExp(find, 'g'),
-		winPath = window.location.pathname.replace(re,'_');
-
-	pageViewTag( 'fashion' + winPath.substring( 0, winPath.lastIndexOf('_') ), element.elementCategory);
-
-	// ===== Init Actions =====
-	$('header, footer').remove();
-
-	(function () {
-		var hash = location.hash;
-
-		if ( hash !== undefined && hash !== '' ) {
-
-			$('#lookbookPage-' + hash.substring(1)).siblings().removeClass('active');
-			$('#lookbookPage-' + hash.substring(1)).fadeIn(400, function() {
-				$('#lookbookPage-' + hash.substring(1)).addClass('active');
-				pageViewTag($('#slideBox .active').attr('id'), element.elementCategory);
-			});
-			
-			
-		} else {
-			if ( $('#lookbooks') ) {
-				$('#lookbooks .slide:first-child').fadeIn(800, function() {
-					$('#lookbooks .slide:first-child').addClass('active');
-					pageViewTag($('#slideBox .active').attr('id'), element.elementCategory);
-				});
-				
-			}
-		}
-
-	})();
-
-
-	
-	// ===== Page Interactions =====
-
-	$('#lookbook, #shopLink').on('click tap', function () {
-		$(this).find('ul').toggle();
-	});
-
-
-
-	var slide = {
-
-		left: function (p, n) {
-
-			if (n < 0) n = $('#slideBox .slide').length - 1;
-
-			$('#slideBox .slide').eq(n).css('left','-100vw').show();
-			var target = $('#slideBox .slide').filter( function (i){
-				return $.inArray(i, [p,n]) > -1;
-			}) ;
-
-			target.animate({'left': '+=100vw'},600, function () {
-				$('#slideBox .active').removeClass('active');
-				$('#slideBox .slide').eq(n).addClass('active');
-				$('.arrow').removeClass('active');
-			});
-			setTimeout(function(){ pageViewTag($('#slideBox .active').attr('id'), element.elementCategory); }, 650);
-		},
-
-		right: function (p, n) {
-
-			if (n >= $('#slideBox .slide').length) n=0;
-
-			$('#slideBox .slide').eq(n).css('left','100vw').show();
-			var target = $('#slideBox .slide').filter( function (i){
-				return $.inArray(i, [p,n]) > -1;
-			}) ;
-
-			target.animate({'left': '-=100vw'},600, function () {
-				$('#slideBox .active').removeClass('active');
-				$('#slideBox .slide').eq(n).addClass('active');
-				$('.arrow').removeClass('active');
-			});
-
-			setTimeout(function(){ pageViewTag($('#slideBox .active').attr('id'), element.elementCategory); }, 650);
-		}
-
-	};
-
-	// clicked on a slide show arrow
-	$('.arrow').on('click tap', function() {
-		var position =  $('#slideBox .active').index(),
-			next;
-
-		if ( !$(this).hasClass('active')) {
-			$('.arrow').addClass('active');
-			if ( $(this).hasClass('left') ) {
-				next = position-1;
-				slide.left(position, next);
-			} else {
-				next = position+1; 
-				slide.right(position, next);
-			}
-		}
-
-	});	
-
-	// Coremetrics 
-
-
-	$('#topNav a').on('click tap', function () {
-		element.elementID = 'topNav-' + $(this).attr('id');
-		elementTag(element);
-	});
-
-	$('#lookbooks .arrow').on('click tap', function () {
-		element.elementID = $('#lookbooks .slide.active').attr('id') + '-' + $(this).attr('id');
-		elementTag(element);
-	});
-
-	$('video').on('play', function () {
-		element.elementID = $(this).attr('id');
-		elementTag(element);
-	});
-
-	function elementTag(element) {
-        return window.cmCreatePageElementTag(element.elementID, element.elementCategory, element.attributes || null);
-    }
-
-    function pageViewTag (pageID, catID){
-
-        window.BLOOMIES.coremetrics.cmCreatePageviewTag(pageID, catID, '', '');        
-    } 
-
-
-
-
 
 */
 
