81cb25dc7e21ed358a70cd5d81de8e0d02c631f3
Joe Orr <joseph.orr@macys.com>
2016-09-27 17:43:42 -0400

    B-55117: centering

--- public/javascripts/bcomLeftNav/about/AboutLeftNav.js
+++ public/javascripts/bcomLeftNav/about/AboutLeftNav.js
@@ -25,8 +25,8 @@ define( [ "LeftNav" ], function ( LeftNav ) {
                 },
                 {
                     text: 'Bloomingdale\'s App',
-                    //href: '/about-us/mobile-shopping-online',
-                    href: '//www1.bloomingdales.com/media/about/mobile.jsp',
+                    href: '/about-us/mobile-shopping-online',
+                    //href: '//www1.bloomingdales.com/media/about/mobile.jsp',
                     id: 'about_onlineMobile',
                     secure: false
                 },
