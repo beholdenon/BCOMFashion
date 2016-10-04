d0fe135ed0e29a84da09236b9bd6a3676729a7f7
Leonardo Brandao <leonardo.brandao@bloomingdales.com>
2016-10-04 18:26:01 -0300

    B-52258 - Registry: Benefits and Perks page - DESKTOP (2)

--- public/javascripts/projects/registry/wedding/benefits-perks/main-desktop.js
+++ public/javascripts/projects/registry/wedding/benefits-perks/main-desktop.js
@@ -4,63 +4,43 @@
 
 'use strict';
 
-var APP = {
-	cm: 'BWEDD_Registry_Home',
-	cmElementCat: 'BWEDD_WHY_REGISTER' //---> coremetrics project prefix
-};
+require([
+    'jquery',
+    'coremetrics',
+], function($, Coremetrics) {
 
-APP.init = function () {
-	var self = this;
+	var APP = {
+		cm: 'BWEDD_Registry_Home',
+		cmElementCat: 'BWEDD_WHY_REGISTER'
+	};
 
-	self.listeners();
-	self.coremetrics('Pageview', self.cm, 'BWEDD_Why_Register');
-};
+	APP.init = function () {
+		var self = this;
 
-APP.listeners = function () {
-	$('.registry-bp-hero-link').on('click', function ( event ){
-		
-		event.preventDefault();
-		
-		var target = $( this ).attr( 'href' );
-		$('html, body').animate({
-            scrollTop: $( target ).offset().top
-        }, 'slow' );
+		self.listeners();
+		Coremetrics.pageViewTag( 'BWEDD_Why_Register', self.cm );
+	};
 
-        APP.coremetrics('Element', APP.cmElementCat, target.substring(1));
+	APP.listeners = function () {
+		$('.registry-bp-hero-link').on('click', function ( event ){
+			
+			event.preventDefault();
+			
+			var target = $( this ).attr( 'href' ),
+				elementCM = {};
+			$('html, body').animate({
+	            scrollTop: $( target ).offset().top
+	        }, 'slow' );
+
+	        elementCM.elementID = target.substring(1);
+	        elementCM.elementCategory = APP.cmElementCat;
+
+	        Coremetrics.elementTag(elementCM);
+		});
+	};
+
+	$(window).load(function() {
+		APP.init();
 	});
-};
 
-//(req) CM business logic
-APP.coremetrics = function (tagType, categoryID, pageID) {
-	var self = this;
-
-	if (tagType === 'Pageview') {
-		try {
-			window.BLOOMIES.coremetrics.cmCreatePageviewTag(pageID, categoryID);
-		} catch (e) {
-			self.logErr('CoreM_err: ' + e);
-		}
-		self.logErr('CoreM ::: tagType: Pageview; categoryID: ' + categoryID + '; pageID: ' + pageID);
-	} else if (tagType === 'Element') {
-		try {
-			window.BLOOMIES.coremetrics.cmCreatePageElementTag(pageID, categoryID);
-		} catch (e) {
-			self.logErr('CoreM_err: ' + e);
-		}
-
-		self.logErr('CoreM ::: tagType: Element; categoryID: ' + categoryID + '; pageID: ' + pageID);
-	}
-};
-
-//(req) custom logger for 'dev' environment
-APP.logErr = function (log) {
-	//log errors only on DEV mode
-	if (window.location.href.indexOf('fashion.bloomingdales.com') < 0) {
-		window.console.info(log);
-	}
-};
-
-
-$(window).load(function() {
-	APP.init();
 });
