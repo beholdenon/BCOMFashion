60122843b7d89e53fba79e40b549734e28c26e41
Alessandro Ribeiro <alessandro.ribeiro@bloomingdales.com>
2016-10-20 19:11:06 -0200

    B-61100 : Create b.cause updates - JDRF - Priority 1 page

---
+++ public/javascripts/projects/about-us/jdrf-b-cause-philanthropy/main.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Alessandro Ribeiro on 10/20/16.
+ */
+
+'use strict';
+
+require([
+    'jquery'
+], function( $ ) {
+
+    var APP = {
+        cmElementCat: 'b_cause_links',
+        cmAttrbute2: '-_-lp-xx-xx-xx.jdrf',
+        cmLinkName: 'jdrf'
+    };
+
+    //(req) CM business logic
+    APP.coremetrics = function (categoryID, pageID) {
+        var self = this;
+
+        try {
+            window.BLOOMIES.coremetrics.cmCreatePageElementTag(pageID, categoryID, APP.cmAttrbute2 );
+        } catch (e) {
+            self.logErr('CoreM_err: ' + e);
+        }
+
+        self.logErr('CoreM ::: tagType: Element; categoryID: ' + categoryID + '; pageID: ' + pageID);
+    };
+
+    //(req) custom logger for 'dev' environment
+    APP.logErr = function (log) {
+        //log errors only on DEV mode
+        if (window.location.href.indexOf('fashion.bloomingdales.com') < 0) {
+            window.console.info(log);
+        }
+    };
+
+    $( window ).load(function() {
+        $('.jdrf-link').on('click', function () {
+            APP.coremetrics(APP.cmElementCat, APP.cmLinkName);
+        });
+	});
+});
