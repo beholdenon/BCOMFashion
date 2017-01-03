74c59d86b9f86f1fd424b364c3d32c526ef93054
u067265 <ely.de.la.cruz@bloomingdales.com>
2017-01-03 17:58:17 -0500

    B-64014 - Mobile About Us Nav - Further progress on setup (added, initial js files, style sheets, and component stylesheet and templates).

---
+++ public/javascripts/projects/about-us/about-us.js
@@ -0,0 +1,17 @@
+/**
+ * Created by u067265 on 1/3/17.
+ */
+/**
+ * Created by u067265 on 11/16/16.
+ */
+require(['jquery', '/fashion/javascripts/components/collapsible-nav'], function ($) {
+
+    'use strict';
+
+    $(function () {
+
+        $('.about-us-nav.collapsible-nav').bcomCollapsibleNav();
+
+    });
+
+});
