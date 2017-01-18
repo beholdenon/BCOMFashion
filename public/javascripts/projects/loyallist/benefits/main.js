4bc4ce572fc9bb2faeed29b8e65cb872b00c74f9
Julio Canario <julio.canario@bloomingdales.com>
2017-01-18 10:47:32 -0500

    FEATURE/B-66275_test JC-Fixing several defects and adding coremetrics

--- public/javascripts/projects/loyallist/benefits/main.js
+++ public/javascripts/projects/loyallist/benefits/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-$(document).ready( function() {
-	$('#learn-more-link').on('click', function () {
-		if ($('.bl_mobile').length) {
-			window.BLOOMIES.coremetrics.cmCreatePageElementTag('MBL: member_benefits-learn_more', 'MBL: Loyallist');
-		} else {
-			window.BLOOMIES.coremetrics.cmCreatePageElementTag('member_benefits-learn_more', 'Loyallist');
-		}
-	});
-
-	require( [ 'jquery', 'bcomLeftNav/account/AccountLeftNav' ], function ( $, accountLeftNav ) {
-	    accountLeftNav.render( '#bl_leftNavContainer', "" );
-	});	
+require( [ 'jquery', 'bcomLeftNav/account/AccountLeftNav' ], function ( $, accountLeftNav ) {
+    accountLeftNav.render( '#bl_leftNavContainer', "" );
+    
+	$(document).ready( function() {
+		$('#learn-more-link').on('click', function () {
+			if ($('.bl_mobile').length) {
+				window.BLOOMIES.coremetrics.cmCreatePageElementTag('MBL: member_benefits-learn_more', 'MBL: Loyallist');
+			} else {
+				window.BLOOMIES.coremetrics.cmCreatePageElementTag('member_benefits-learn_more', 'Loyallist');
+			}
+		});
+	});    
 });
