18ab936ce81d3298983c16642c9f913bd44cadbb
Michael Curtis <michael.curtis@bloomingdales.com>
2017-02-14 17:45:17 -0500

    B-70518 Updating requireJS configuration to include /b/ proxy

--- public/javascripts/require_config.js
+++ public/javascripts/require_config.js
@@ -1,15 +1,15 @@
 (function() {
     require.config({
         paths: { 
-            'coremetrics': '/fashion/javascripts/includes/coremetrics',
-            'globalns': '/fashion/javascripts/includes/globalns',
-            'globalHeaderFooter': '/fashion/javascripts/includes/globalHeaderFooter',
-            'lodash' : '/fashion/javascripts/libs/lodash.4',
-            'nav' : '/fashion/javascripts/includes/nav',
-            'propsConfig': '/fashion/javascripts/includes/propsConfig',
-            'partialDataHF': '/fashion/javascripts/includes/partialDataHF',
-            'pressReleases': '/fashion/javascripts/projects/about-us/company/press',
-            'templates': '/fashion/templates'
+            'coremetrics': '/b/fashion/javascripts/includes/coremetrics',
+            'globalns': '/b/fashion/javascripts/includes/globalns',
+            'globalHeaderFooter': '/b/fashion/javascripts/includes/globalHeaderFooter',
+            'lodash' : '/b/fashion/javascripts/libs/lodash.4',
+            'nav' : '/b/fashion/javascripts/includes/nav',
+            'propsConfig': '/b/fashion/javascripts/includes/propsConfig',
+            'partialDataHF': '/b/fashion/javascripts/includes/partialDataHF',
+            'pressReleases': '/b/fashion/javascripts/projects/about-us/company/press',
+            'templates': '/b/fashion/templates'
         }
     });  
 })();
