3b973f2b3e0eda855d66f3442894cf9a2a24a611
Julio Canario <julio.canario@bloomingdales.com>
2017-03-06 16:48:50 -0500

    FEATURE/B-65074_2_master JC-Removing back to top button html js and css

--- public/javascripts/projects/lookbooks/syndeca-projects/main-desktop.js
+++ public/javascripts/projects/lookbooks/syndeca-projects/main-desktop.js
@@ -2,38 +2,9 @@ window.SYNDECA = window.SYNDECA || {};
 
 window.SYNDECA.vertical = (function(window, document, $) {
     'use strict';
-    var isTablet = $('body').hasClass('bl_tablet') ? true : false;
-
-    //back to top listener
-    $('.desktop_back_to_top, #syndeca_mobile_back_to_top_button').on('click', function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 'slow' );
-    });
-
-    $( window ).scroll(function() {
-        if( !isTablet ) {
-            checkHeightAndShowBackToTop();
-        }
-    });
 
     $(window).load(function() {
-		    	
         window.BLOOMIES.coremetrics.cmCreatePageviewTag('SyndecaLookBook', 'SyndecaLookBook');
-        if( !isTablet ) {
-            checkHeightAndShowBackToTop();
-        }
-
     });
-
-    function checkHeightAndShowBackToTop(){
-        var syndecaWrapper = $('#syndeca'),
-            syndecaHeight = syndecaWrapper.height();
-
-        if(syndecaHeight >= 1000) {
-            var backToTopBtn = $('.desktop_back_to_top');
-            backToTopBtn.show();
-        }
-    }
 
 }(window, document, jQuery));
