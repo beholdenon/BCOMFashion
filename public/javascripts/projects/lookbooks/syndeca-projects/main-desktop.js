3aac350f00f4636b1699859e1dc09d8f597293d0
Julio Canario <julio.canario@bloomingdales.com>
2017-04-11 14:14:55 -0400

    FEATURE/B-75289 JC-Reverting back-to-top button code for desktop

--- public/javascripts/projects/lookbooks/syndeca-projects/main-desktop.js
+++ public/javascripts/projects/lookbooks/syndeca-projects/main-desktop.js
@@ -2,9 +2,32 @@ window.SYNDECA = window.SYNDECA || {};
 
 window.SYNDECA.vertical = (function(window, document, $) {
     'use strict';
-
-    $(window).load(function() {
-        window.BLOOMIES.coremetrics.cmCreatePageviewTag('SyndecaLookBook', 'SyndecaLookBook');
+    var isTablet = $('body').hasClass('bl_tablet') ? true : false;
+    //back to top listener
+    $('.desktop_back_to_top').on('click', function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 'slow' );
     });
+    $( window ).scroll(function() {
+        if( !isTablet ) {
+            checkHeightAndShowBackToTop();
+        }
+    });
+    $(window).load(function() {
+		    	
+        window.BLOOMIES.coremetrics.cmCreatePageviewTag('SyndecaLookBook', 'SyndecaLookBook');
+        if( !isTablet ) {
+            checkHeightAndShowBackToTop();
+        }
+    });
+    function checkHeightAndShowBackToTop(){
+        var syndecaWrapper = $('#syndeca'),
+            syndecaHeight = syndecaWrapper.height();
+        if(syndecaHeight >= 1000) {
+            var backToTopBtn = $('.desktop_back_to_top');
+            backToTopBtn.show();
+        }
+    }
 
 }(window, document, jQuery));
