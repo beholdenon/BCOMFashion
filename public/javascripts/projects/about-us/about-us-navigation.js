b3bc612efe48d57e334c933f6f9ca8b672cbb386
Julio Canario <julio.canario@bloomingdales.com>
2017-04-23 12:06:46 -0400

    FEATURE/D-51223-2 JC-Removing duplicate CM tags and setting selected subnavs to active

--- public/javascripts/projects/about-us/about-us-navigation.js
+++ public/javascripts/projects/about-us/about-us-navigation.js
@@ -24,7 +24,8 @@ require([
             $aboutUsExpandLink = $aboutUsLink.next(),
             aboutUsElementIdForOpen = $aboutUsLink.data('cm-element-id-open'),
             aboutUsElementCatIdForOpen = $aboutUsLink.data('cm-element-category-id'),
-            aboutUsExploreAttributes = new window.BLOOMIES.coremetrics.exploreAttributes({2: $('#cmdata').attr('data-pageid')});
+            aboutUsExploreAttributes = new window.BLOOMIES.coremetrics.exploreAttributes({2: $('#cmdata').attr('data-pageid')}),
+            sizeChart = $('#sizeChart');
             
             [$aboutUsLink, $aboutUsExpandLink].forEach(function ($elm) {
                 $elm.on('click', function () {
@@ -35,6 +36,12 @@ require([
                     });
                 });
             });
+
+            //When user selects Size Chart page it must be set to "active" only after DOM has been created.
+            //TODO: Rewrite and move this logic into aboutUsNavFactoryForMobile.js
+            if (window.location.href.indexOf('/sizecharts/') > -1 && sizeChart && !sizeChart.hasClass('active')) {
+                sizeChart.addClass('active');
+            }
 
     });
 
