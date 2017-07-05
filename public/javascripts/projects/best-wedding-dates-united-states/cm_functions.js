4626fb35553a9cb90785c1253221610b620cf6ae
Michael Curtis <michael.curtis@bloomingdales.com>
2017-07-05 11:48:24 -0400

    b-66901 fixing jsHint errors from cm functions

--- public/javascripts/projects/best-wedding-dates-united-states/cm_functions.js
+++ public/javascripts/projects/best-wedding-dates-united-states/cm_functions.js
@@ -1,29 +1,32 @@
+'use strict';
+/* globals BLOOMIES */
+
 (function(){
 	
   var hasMBL = ( window.BLOOMIES.isMobile === true ? "mbl:" : ""); 
-  var cm_cat = hasMBL + 'BWEDD_Best_Wedding_Dates_US';
+  var cmCat = hasMBL + 'BWEDD_Best_Wedding_Dates_US';
 
   $('#methodology_open_btn').on('click', function() {
-    BLOOMIES.coremetrics.cmCreateConversionEventTag(hasMBL +  'How_does_this_work' , 1, cm_cat);
+    BLOOMIES.coremetrics.cmCreateConversionEventTag(hasMBL +  'How_does_this_work' , 1, cmCat);
   });
 
   $('#methodology_close_btn').on('click', function() {
-    BLOOMIES.coremetrics.cmCreateConversionEventTag(hasMBL +  'How_does_this_work' , 2, cm_cat);
+    BLOOMIES.coremetrics.cmCreateConversionEventTag(hasMBL +  'How_does_this_work' , 2, cmCat);
   });
 
   $('a.social_btn').on('click', function() {
-    BLOOMIES.coremetrics.cmCreatePageElementTag(hasMBL +  'BWEDD_Share-{' + $(this).find('.social-likes__button').text() + '}' , cm_cat);
+    BLOOMIES.coremetrics.cmCreatePageElementTag(hasMBL +  'BWEDD_Share-{' + $(this).find('.social-likes__button').text() + '}' , cmCat);
   });
 
 	$(window).on('calendarLoad', function() {
     var locationFormat = $('#city_autocomplete').val();
     locationFormat = locationFormat.replace(/,\s/g,'_').replace(/\s/g,'-');
 
-    window.BLOOMIES.coremetrics.cmCreatePageElementTag(hasMBL + 'BWEDD_Search-{' + locationFormat + '}', cm_cat);
+    window.BLOOMIES.coremetrics.cmCreatePageElementTag(hasMBL + 'BWEDD_Search-{' + locationFormat + '}', cmCat);
   });
 
   $(window).load(function() {
-    window.BLOOMIES.coremetrics.cmCreatePageviewTag(hasMBL + 'BWEDD_Best_Wedding_Dates_US', cm_cat);
+    window.BLOOMIES.coremetrics.cmCreatePageviewTag(hasMBL + 'BWEDD_Best_Wedding_Dates_US', cmCat);
   });
 
 })();
