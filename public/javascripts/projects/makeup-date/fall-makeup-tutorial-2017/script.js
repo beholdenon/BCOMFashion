901a2d575f3fd8f04dadefba95688c2b978b4a00
Michael Curtis <michael.curtis@bloomingdales.com>
2017-07-31 17:46:31 -0400

    B-86675 Completing basic carousel functionality

--- public/javascripts/projects/makeup-date/fall-makeup-tutorial-2017/script.js
+++ public/javascripts/projects/makeup-date/fall-makeup-tutorial-2017/script.js
@@ -1,7 +1,7 @@
+/* globals SERVICES*/
 'use strict';
 
 (function($) {
-
 
   // REMOVE BEFORE TEST
   $("#floating-nav").remove();
@@ -30,33 +30,97 @@
       });
     },
 
+
+    // Housing div required to have data-items as comma separated items and a 
+    // group size attribute that informs how many items per group
     setupCarousel: function (e) {
-      var products = $(e).attr('data-items').split(',');
-      var groupSize = $(e).attr("data-groupSize");
-      var groups = Math.ceil( products.length / groupSize );
+      var products = $(e).attr('data-items');
+      var productArray = products.split(',');
+      var groupSize = parseInt( $(e).attr("data-groupSize") );
+      var totalGroups = Math.ceil( productArray.length / groupSize );
+      var size;
 
-      // create groups
-      for ( var g = 0; g < groups; g++ ) {
+      switch (groupSize) {
+        case 1: 
+          size = 'one';
+          break;
+        case 2: 
+          size = 'two';
+          break;
+        case 3: 
+          size = 'three';
+          break;
+        case 4: 
+          size = 'four';
+          break;
+        case 5: 
+          size = 'five';
+          break;
+        default:
+          size = 'three';
+      }
 
-        $( '<div class="group"></div>' ).appendTo( $(e) );
+      // create group shells
+      for ( var g = 0; g < totalGroups; g++ ) {
+
+        $( '<div class="group '+ size +' " data-num="'+g+'"></div>' ).appendTo( $(e) );
 
         if ( g === 0 ) {
           $(e).children().eq(g).addClass('active');
-        } else if ( g === $(e).children().length - 1 && $(e).children().length > 2 ) {
-          $(e).children().eq(g).addClass('last');
+        } else {
+          $(e).children().eq(g).prop('tabindex', "-1");
         }
       }
 
-      // create products append to appropriate group
-      for ( var p = 0; p < products.length; p++ ) {
+      $( '<div class="arrow leftArrow" tabindex="0"></div>' ).prependTo( $(e) );
+      $( '<div class="arrow rightArrow" tabindex="0"></div>' ).appendTo( $(e) );
 
-        var markup = '<div class="product" data-upc='+products[p]+'>';
-        markup += '<img src="" alt="" />';
-        markup += '<h4>Item Name</h4>';
-        markup += '<p>Item Description</p>';
-        markup += '</div>';
+      // get Data based on UPC list
+      SERVICES.product.upcGet( function (res) {
+        var resArray = res.product;
 
-        $( markup ).appendTo( $(e).find('.group').eq( Math.floor( p/groupSize ) ) );
+        // create products append to appropriate group after AJAX data request
+        for ( var p = 0; p < resArray.length; p++ ) {
+
+          var brand = resArray[p].productDetails.summary.brand;
+          var name = resArray[p].productDetails.summary.name.replace( brand, '');
+          // if ( name.length > 44 ) name = name.substring(0,41) + '...';
+
+          var markup = '<a href="'+resArray[p].productDetails.summary.productURL+'"class="product" data-id=' + resArray[p].id + '>';
+          markup += '<img src="https://images.bloomingdales.com/is/image/BLM/products/4/optimized/'+resArray[p].productDetails.summary.primaryPortraitSource+'" alt="" />';
+          markup += '<h4>'+ brand +'</h4>';
+          markup += '<p>'+ name +'</p>';
+          markup += '</a>';
+
+          $( markup ).appendTo( $(e).find('.group').eq( Math.floor( p/groupSize ) ) );
+        }
+
+      }, products );
+
+      
+
+    },
+
+    rotate: function (ele, dir) {
+      var groupLength = ele.find('.group').length;
+      var active = ele.find('.group.active').index();
+
+      console.log( active );
+
+      ele.find('.active').removeClass('active').prop('tabindex', "-1");
+
+      if ( dir === 'left' ) {
+        if ( active - 1 < 0 ) {
+          ele.find('.group').eq( groupLength - 1 ).addClass('active').prop('tabindex', 0);
+        } else {
+          ele.find('.group').eq( active - 1 ).addClass('active').prop('tabindex', 0);
+        }
+      } else if ( dir === 'right' ) {
+        if ( active + 1 >= groupLength ) {
+          ele.find('.group').eq( 0 ).addClass('active').prop('tabindex', 0);
+        } else {
+          ele.find('.group').eq( active + 1 ).addClass('active').prop('tabindex', 0);
+        }
       }
 
     },
@@ -64,5 +128,21 @@
   };
 
   carousel.init();
+
+  $('.carousel-shell').on('click tap', '.arrow', function() {
+
+    if ( $(this).hasClass('leftArrow') ) {
+      carousel.rotate( $(this).parents('.carousel-shell'), 'left' );
+    } else {
+      carousel.rotate( $(this).parents('.carousel-shell'), 'right' );
+    }
+
+  });
+
+  $('document').keypress( '.arrow', function(ev) {
+    if ( ev.keyCode === 13 ) {
+      $(this).click();
+    }
+  });
 
 })(jQuery);
