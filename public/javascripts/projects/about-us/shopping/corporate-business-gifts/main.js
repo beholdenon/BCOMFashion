145c1a2b77b44d16df0be5ce9b583ec6374ea39d
daniel.lanna@bloomingdales.com <daniel.lanna@bloomingdales.com>
2017-09-08 16:13:08 -0300

    B-85365: Business Gifts - holiday refresh

--- public/javascripts/projects/about-us/shopping/corporate-business-gifts/main.js
+++ public/javascripts/projects/about-us/shopping/corporate-business-gifts/main.js
@@ -4,57 +4,26 @@
 
 'use strict';
 
-require([
-    'jquery'
-], function( $ ) {
+require([ 'jquery' ], function( $ ) {
 
-    var APP = {};
+    $(document).ready(function() {
 
-    //(req) CM business logic
-    APP.coremetrics = function (categoryID, pageID, attributes) {
-        var self = this;
-
-        if ( $('.bl_mobile').length > 0 ){
-            categoryID = 'MBL:' + categoryID;
-            pageID = 'MBL:' + pageID;
-        }
-
-        try {
-            window.BLOOMIES.coremetrics.cmCreatePageElementTag(pageID, categoryID, attributes);
-        } catch (e) {
-            self.logErr('CoreM_err: ' + e);
-        }
-
-        self.logErr('CoreM ::: tagType: Element; categoryID: ' + categoryID + '; pageID: ' + pageID);
-    };
-
-    //(req) custom logger for 'dev' environment
-    APP.logErr = function (log) {
-        //log errors only on DEV mode
-        if (window.location.href.indexOf('fashion.bloomingdales.com') < 0) {
-            window.console.info(log);
-        }
-    };
-
-    $( window ).load(function() {
-        var isMobile =  $('.bl_mobile').length,
-            cmElementCat = 'Business_Gifts',
-            cmElementId = 'business_gifts_stores',
-            cmAttrbute2 = '-_-Corporate_Services_(ab-sh-xx-xx.corporate)',
+        var isMobile = window.BLOOMIES.isMobile,
+            cmElementCat = isMobile ? 'mbl: Business_Gifts' : 'Business_Gifts',
+            cmElementId = isMobile ? 'mbl: business_gifts_stores' : 'business_gifts_stores',
+            cmAttrbutes = '-_-Corporate_Services_(ab-sh-xx-xx.corporate)',
             serverDate = new Date( $('input[name="timeStamp"]').val() ),
-            expirationDate = new Date(2016,11,26,0,1);
-
+            expirationDate = new Date(2017,11,24,0,1);
+        
         if ( serverDate >= expirationDate ) {
-            $('.holiday').remove();
+            $('.shop-holiday').remove();
         }
 
-        $('.store-top, .store-bottom').on('click', function () {
+        $('.contact-details').on('click', function () {
             var link = $( this ),
-                className = link.attr('class'),
-                elementId = cmElementId + (className.indexOf( 'top' ) !== -1 ? '_top_' : '_bottom_') + link.data('elementId');
+                elementId = cmElementId + '_top_'+ link.data('elementId');
 
-            if ( !isMobile && link.hasClass( 'phone-number' ) ) { return; }
-            APP.coremetrics(cmElementCat, elementId, cmAttrbute2);
+            window.BLOOMIES.coremetrics.cmCreatePageElementTag(elementId, cmElementCat, cmAttrbutes);
         });
 	});
 });
