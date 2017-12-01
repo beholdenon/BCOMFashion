3e23b7bb2b4dfd4bdb9305db8a74af1a968eea33
Alex Maltsev <Alex.Maltsev@bloomingdales.com>
2017-12-01 15:59:06 -0500

    B-97529 - Resolve merge conflict
    B-97529 - Resolve merge conflict.

--- campaigns/campaign_routes.js
+++ campaigns/campaign_routes.js
@@ -1,12 +1,8 @@
 module.exports = [
-  { method: 'GET',  path: '/b/campaigns/spring-fashion-trends/2018/{path*}',    config: require('../lib/handlers/views').responsiveCustomHF }
-    
-  //{ method: 'GET',  path: '/b/campaigns/spring-fashion-trends/2018/{path*}',    config: require('../server/lib/handlers/views').adaptive }
-  
-  
-  
-  //{ method: 'GET',  path: '/b/campaigns/example-page/{path*}',      config: require(__dirname + '/../lib/handlers/views').adaptive },
-    
+  { method: 'GET',  path: '/b/campaigns/spring-fashion-trends/2018/{path*}',    config: require('../lib/handlers/views').responsiveCustomHF },
+  { method: 'GET',  path: '/b/campaigns/example-page/{path*}',                config: require(__dirname + '/../lib/handlers/views').adaptive },
+  { method: 'GET',  path: '/b/campaigns/makeup-date/makeup-tutorial/{path*}', config: require(__dirname + '/../lib/handlers/views').adaptive },
+
   // EXAMPLES
   //{ method: 'GET',  path: '/campaigns/example-page/{path*}',    config: require('../server/lib/handlers/views').adaptive },
   //{ method: 'GET',  path: '/campaigns/example-page/{path*}',    config: require('./lib/handlers/views').responsiveCustomHF },
