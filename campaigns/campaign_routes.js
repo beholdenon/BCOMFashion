318ee943a8a6f473cb52dacb0ee253a85e72edcd
Alex Maltsev <Alex.Maltsev@bloomingdales.com>
2018-01-30 12:22:47 -0500

    B-102450 Men’s page upadte
    B-102450 Men’s page upadte

--- campaigns/campaign_routes.js
+++ campaigns/campaign_routes.js
@@ -1,4 +1,5 @@
 module.exports = [
+  { method: 'GET',  path: '/b/campaigns/2018-spring-campaign-100-percent-exclusive/{path*}', config: require('../lib/handlers/views').responsiveCustomHF },
   { method: 'GET',  path: '/b/campaigns/spring-fashion-trends/2018/{path*}',    config: require('../lib/handlers/views').responsiveCustomHF },
   { method: 'GET',  path: '/b/campaigns/example-page/{path*}',                config: require(__dirname + '/../lib/handlers/views').adaptive },
   { method: 'GET',  path: '/b/campaigns/makeup-date/makeup-tutorial_old/{path*}', config: require(__dirname + '/../lib/handlers/views').adaptive },
