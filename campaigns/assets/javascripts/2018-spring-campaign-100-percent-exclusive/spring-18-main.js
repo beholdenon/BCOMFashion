2560b639c966bc948920a7899f50f04e67e1ce64
Alex Maltsev <Alex.Maltsev@bloomingdales.com>
2018-02-08 12:18:57 -0500

    B-102450 - Update Nintendo page
    B-102450 - Update Nintendo page

--- campaigns/assets/javascripts/2018-spring-campaign-100-percent-exclusive/spring-18-main.js
+++ campaigns/assets/javascripts/2018-spring-campaign-100-percent-exclusive/spring-18-main.js
@@ -1,36 +1,36 @@
 $(function () {
 
-    $.fn.fitText = function( kompressor, options ) {
-
-        // Setup options
-        var compressor = kompressor || 1,
-            settings = $.extend({
-                'minFontSize' : Number.NEGATIVE_INFINITY,
-                'maxFontSize' : Number.POSITIVE_INFINITY
-            }, options);
-
-        return this.each(function(){
-
-            // Store the object
-            var $this = $(this);
-
-            // Resizer() resizes items based on the object width divided by the compressor * 10
-            var resizer = function () {
-                $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-            };
-
-            // Call once to set.
-            resizer();
-
-            // Call on resize. Opera debounces their resize by default.
-            $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-        });
-
-    };
-    // $("#responsive_headline").fitText(1.2); // Turn the compressor up   (resizes more aggressively)
+    // $.fn.fitText = function( kompressor, options ) {
+    //
+    //     // Setup options
+    //     var compressor = kompressor || 1,
+    //         settings = $.extend({
+    //             'minFontSize' : Number.NEGATIVE_INFINITY,
+    //             'maxFontSize' : Number.POSITIVE_INFINITY
+    //         }, options);
+    //
+    //     return this.each(function(){
+    //
+    //         // Store the object
+    //         var $this = $(this);
+    //
+    //         // Resizer() resizes items based on the object width divided by the compressor * 10
+    //         var resizer = function () {
+    //             $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+    //         };
+    //
+    //         // Call once to set.
+    //         resizer();
+    //
+    //         // Call on resize. Opera debounces their resize by default.
+    //         $(window).on('resize.fittext orientationchange.fittext', resizer);
+    //
+    //     });
+    //
+    // };
+    //$(".responsive-headline").fitText(0.5); // Turn the compressor up   (resizes more aggressively)
     // $("#responsive_headline").fitText(0.8); // Turn the compressor down (resizes less aggressively)
-    // $("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px' });
+    //$(".responsive-headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '52px' });
 
     // Prevent tabbing out of the nav items when nav is active
     var navItem = $('.spring-18-nav a'),
@@ -117,7 +117,7 @@ $(function () {
     });
 
     
-    console.log('spring-18-carousel-images: ' + $('.spring-18-carousel-images div').length);
+    //console.log('spring-18-carousel-images: ' + $('.spring-18-carousel-images div').length);
     
     
     // Landing page video
